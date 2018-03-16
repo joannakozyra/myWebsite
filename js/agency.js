63b0c78d8f9c1bad5d6d66ca81ac288034a4ee2c
Lateralus <michal.czerwien@gmail.com>
2018-03-16 11:20:15 +0100

    Basic frame, styling

---
+++ js/agency.js
@@ -0,0 +1,13 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Hide navbar when modals trigger
+  $('.portfolio-modal').on('show.bs.modal', function(e) {
+    $(".navbar").addClass("d-none");
+  })
+  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+    $(".navbar").removeClass("d-none");
+  })
+
+  
+})(jQuery); // End of use strict
