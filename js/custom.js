3b93251c0d8d9b3bef32d4cdcbc95c89599643c4
Lateralus <michal.czerwien@gmail.com>
2018-03-26 11:50:52 +0200

    navigation top and mobile

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top - 170
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
